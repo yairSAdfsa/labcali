151d1ea4e75295cdfe45f47e8e31ac9df743ccc5
yairSAdfsa <yair.araujo@tecsup.edu.pe>
2025-09-17 15:28:19 -0500

    Proyecto terminado 20 xd

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for mascotas project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from gestor import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.listar_mascotas, name='listar_mascotas'),
+    path('crear/', views.crear_mascota, name='crear_mascota'),
+    path('editar/<int:mascota_id>/', views.editar_mascota, name='editar_mascota'),
+    path('eliminar/<int:mascota_id>/', views.eliminar_mascota, name='eliminar_mascota'),
 ]
